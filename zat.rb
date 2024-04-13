43d4cd0b7cf9df79caf9a6e23626fd16ebbb77cf
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2024-04-14 09:18:59 +1000

    Bump

--- zat.rb
+++ zat.rb
@@ -5,9 +5,9 @@
 class Zat < Formula
   desc "Simple project templating system"
   homepage "https://github.com/ssanj/zat"
-  url "https://github.com/ssanj/zat/archive/refs/tags/v0.11.10.tar.gz"
+  url "https://github.com/ssanj/zat/archive/refs/tags/v0.11.12.tar.gz"
   version "0.11.10"
-  sha256 "3705bcd7c47e500b2682653b95d073e446c7bf1f7e7719056c63d0442607eb7e"
+  sha256 "0663752ade36143c5ca8d5cd6644e61e984bfce80e077b33bd7fd0581a8588ce"
   license "BSD-3-Clause"
 
   depends_on "rust" => :build
